7b4c323795dffebc6b2f9a030c27ff2c370bdda8
Mubi9000 <shabbirmuhammad2000@gmail.com>
2023-08-02 17:52:48 -0400

    Volume bar now changes volume and can be adjusted

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const progressBar = document.querySelector('.progress-bar')
 const playBtn = document.getElementById('play-btn')
 const volumeIcon = document.getElementById('volume-icon')
 const volumeRange = document.querySelector('.volume-range')
-const volumeBar = document.querySelector('volume-bar')
+const volumeBar = document.querySelector('.volume-bar')
 const currentTime = document.querySelector('.time-elapsed')
 const duration = document.querySelector('.time-duration')
 const fullscreenBtn = document.querySelector('.fullscreen')
@@ -59,7 +59,29 @@ function setProgress(e) {
 
 // Volume Controls --------------------------- //
 
-
+// Volume Bar
+function changeVolume(e) {
+  let volume = e.offsetX / volumeRange.offsetWidth
+  // Rounding volume up or down
+  if(volume < 0.1) {
+    volume = 0
+  }
+  if (volume > 0.9) {
+    volume = 1
+  }
+  volumeBar.style.width = `${volume * 100}%`
+  video.volume = volume
+  console.log(volume)
+  // Change icon depending on volume
+  volumeIcon.className = ''
+  if (volume > 0.7) {
+    volumeIcon.classList.add('fas', 'fa-volume-up')
+  } else if (volume < 0.7 && volume > 0) {
+    volumeIcon.classList.add('fas', 'fa-volume-down')
+  } else if (volume === 0) {
+    volumeIcon.classList.add('fas', 'fa-volume-mute')
+  }
+}
 
 // Change Playback Speed -------------------- //
 
@@ -73,3 +95,4 @@ video.addEventListener('click', togglePlay)
 video.addEventListener('timeupdate', updateProgress)
 video.addEventListener('canplay',updateProgress)
 progressRange.addEventListener('click', setProgress)
+volumeRange.addEventListener('click', changeVolume)
